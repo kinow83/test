0cc8070eb98900632101a622f1c4118470c8b1ed
root <kinow83@gmail.com>
2017-08-18 18:51:04 +0900

    ...ing

---
+++ src/mail/send.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export MAIL_VERBOSE=1
+export MAIL_DEBUG=1
+
+sendmail -f kinow@naver.com kinow83@gmail.com << EOF
+FROM: kinow@naver.com
+To: kinow83@gmail.com
+Subject: kakakakakakakaka
+wowowowowowowowowowowowowow
+wowowowowowowowowowowowowow
+wowowowowowowowowowowowowow
+.
+EOF
