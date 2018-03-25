29eeec8139bcc5f9d056244369bab444a792f3f7
kinow83 <kinow83@gmail.com>
2018-03-25 20:28:27 +0900

    test

--- src/ip-longest-prefix/test_linear_ip.c
+++ src/ip-longest-prefix/test_linear_ip.c
@@ -6,17 +6,30 @@ typedef struct ipsubnet {
 	uint8_t cidr;
 } ipsubnet;
 
-static ipsubnet subnets[128];
+#define MAX_SUBNET	0xFFFF
+static ipsubnet subnets[MAX_SUBNET];
 static int subnet_len = 0;
 
 void add_ipsubnet(ipsubnet *s)
 {
-	subnets[subnet_len++] = *s;
+	subnet_len = (subnet_len + 1) % MAX_SUBNET;
+	subnets[subnet_len] = *s;
 }
 
 int match_ipsubnet(uint32_t ip)
 {
+	int i;
+	ipsubnet *s;
+	for (i=0; i<subnet_len; i++) {
+		s = &subnets[i];
+		if (s->cidr == 32) {
+			if (ip == s->ip) {
+				return 1;
+			}
+		} else {
 
+		}
+	}
 }
 
 int main()
