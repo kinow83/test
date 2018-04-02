af29153da0b78baa2adfe987b6411e69d78638c5
kinow83 <kinow83@gmail.com>
2018-04-02 22:29:30 +0900

    clean

--- src/hash/my/agent/include/port.h
+++
@@ -1,6 +0,0 @@
-/*
-* @file	: port.h
-* @brief: 포트 정보
-*/
-#define MASTER_PORT 9000			// 마스터와 통신 포트
-
--- src/hash/test_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TC=100MB.txt
-
-./gnu/hsearch $TC
-./gnu/unordered_map $TC
-./google/sparsehash-master/simple_test $TC
-
